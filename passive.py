a6ec1c808d5a47554a94c9d0f775e82d4e0d4b68
nsankethreddy <sankethn1947@gmail.com>
2021-06-02 11:37:24 +0000

    changed passive file

--- passive.py
+++ passive.py
@@ -16,37 +16,32 @@ channel = connection.channel()
 connection2 = pika.BlockingConnection(pika.ConnectionParameters(host = "active",port=5672,credentials=cred))
 channel2 = connection2.channel()
 
-
-
 client_params = {"x-ha-policy": "all"}
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-class PassiveServer:
-    def __init__(self):
-        receive_from_clients = threading.Thread(target=self.messages_from_clients)
+def PassiveServer():
+        receive_from_clients = threading.Thread(target=messages_from_clients)
         receive_from_clients.daemon = True
         receive_from_clients.start()
-
-        receive_from_active = threading.Thread(target=self.messages_from_active)
+        receive_from_active = threading.Thread(target=messages_from_active)
         receive_from_active.daemon = True
         receive_from_active.start()
     
-    def broadcast(body, props):
+def broadcast(body, props):
         channel.basic_publish(exchange='logs', routing_key='',body=body, properties= pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
 
-    def messages_from_clients():
+def messages_from_clients():
         channel.queue_declare(queue='hello',durable=True,arguments=client_params)
 
         def callback(ch, method, properties, body):
              print(" [x] %s" % body.decode())
              broadcast(body, properties)
 
-        channel.basic_consume(
-              queue='hello', on_message_callback=callback, auto_ack=True)
+        channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
         print(channel.channel_number,' [*] Waiting for messages. To exit press CTRL+C')
         channel.start_consuming()
     
-    def messages_from_active():
+def messages_from_active():
         channel2.queue_declare(queue='hello',durable=True,arguments=client_params)
 
         def callback(ch, method, properties, body):
@@ -58,18 +53,12 @@ class PassiveServer:
         print(' [*] Waiting for messages. To exit press CTRL+C')
         channel2.start_consuming()
 
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
+    
     try:
-
-        passive_server = PassiveServer()
+        PassiveServer()
+        while 1:
+            pass
 
     except KeyboardInterrupt:
         print('Interrupted')
