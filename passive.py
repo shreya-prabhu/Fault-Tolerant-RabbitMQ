a1523400da40fd73ab32f453b934558d46190f8e
nsankethreddy <sankethn1947@gmail.com>
2021-06-02 16:49:02 +0000

    minor changes

--- passive.py
+++ passive.py
@@ -1,6 +1,7 @@
 import pika
 import sys
 import os
+import threading
 
 IP = 'localhost'
 server = ''
@@ -10,6 +11,9 @@ cred = pika.PlainCredentials('admin', 'password')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host = "localhost",port=5672,credentials=cred))
 channel = connection.channel()
 
+connection2 = pika.BlockingConnection(pika.ConnectionParameters(host = "active",port=5672,credentials=cred))
+channel2 = connection2.channel()
+
 client_params = {"x-ha-policy": "all"}
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
@@ -17,8 +21,25 @@ channel.exchange_declare(exchange='logs', exchange_type='fanout')
 def broadcast(body, props):
     channel.basic_publish(exchange='logs', routing_key='',body=body, properties= pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
 
+def from_active():
+      try:
+        channel2.queue_declare(queue='active-passive',durable=True,arguments=client_params)
 
-def main():
+        def callback(ch, method, properties, body):
+             print(" [x] %s" % body.decode())
+       
+
+        channel2.basic_consume(
+            queue='active-passive', on_message_callback=callback, auto_ack=True)
+       # print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel2.start_consuming()
+
+      except:
+        print("Active server has crashed!")
+   
+
+
+def from_client():
     channel.queue_declare(queue='hello',durable=True,arguments=client_params)
 
     def callback(ch, method, properties, body):
@@ -27,14 +48,24 @@ def main():
 
     channel.basic_consume(
         queue='hello', on_message_callback=callback, auto_ack=True)
-    print(channel.channel_number,' [*] Waiting for messages. To exit press CTRL+C')
+    print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 
 if __name__ == '__main__':
     try:
 
-        main()
+        receive_from_active = threading.Thread(target=from_active)
+        receive_from_active.daemon = True
+        receive_from_active.start()
+
+        receive_from_client = threading.Thread(target=from_client)
+        receive_from_client.daemon = True
+        receive_from_client.start()
+
+        receive_from_active.join()
+        receive_from_client.join()
+        
     except KeyboardInterrupt:
         print('Interrupted')
         try:
