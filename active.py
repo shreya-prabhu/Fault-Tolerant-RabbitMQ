75cbf7601c042dffda99dc293464db9ae72ff48b
gaurika poplai <gaurikapoplai@gmail.com>
2021-06-02 17:30:43 +0530

    threading in active

--- active.py
+++ active.py
@@ -3,6 +3,9 @@
 import pika
 import sys
 import os
+import threading
+import time
+import uuid
 
 IP = 'localhost'
 server = ''
@@ -19,19 +22,23 @@ connection2 = pika.BlockingConnection(pika.ConnectionParameters(host = "passive"
 channel2 = connection2.channel()
 
 
+   
+
+
+
 def broadcast(body, props):
     channel.basic_publish(exchange='logs', routing_key='',body=body, properties = pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
 
 def sendtopassive(body, props):
     channel2.basic_publish(exchange='', routing_key='hello', properties=pika.BasicProperties(correlation_id=props.correlation_id,body=body,delivery_mode = 2))
 
-def main():
+def messages_from_clients():
     channel.queue_declare(queue='hello',durable=True,arguments=client_params)
 
     def callback(ch, method, properties, body):
         print(" [x] %s" % body.decode())
         broadcast(body, properties)
-        sendtopassive(body,properties)
+        #sendtopassive(body,properties)
 
     channel.basic_consume(
         queue='hello', on_message_callback=callback, auto_ack=True)
@@ -41,8 +48,14 @@ def main():
 
 if __name__ == '__main__':
     try:
+       receive_from_clients = threading.Thread(target=messages_from_clients)
+       receive_from_clients.daemon = True
+       receive_from_clients.start()
+       send_to_passive = threading.Thread(target=sendtopassive)
+       send_to_passive.daemon = True
+       send_to_passive.start()
+        
 
-        main()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
