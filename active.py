2213ddc276ebe16b5bcb1466246bf367386965ab
nsankethreddy <sankethn1947@gmail.com>
2021-06-02 18:20:20 +0530

    tried resetting

--- active.py
+++ active.py
@@ -3,6 +3,9 @@
 import pika
 import sys
 import os
+import threading
+import time
+import uuid
 
 IP = 'localhost'
 server = ''
@@ -15,12 +18,16 @@ channel = connection.channel()
 client_params = {"x-ha-policy": "all"}
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
+connection2 = pika.BlockingConnection(pika.ConnectionParameters(host = "passive",port=5672,credentials=cred))
+channel2 = connection2.channel()
 
 def broadcast(body, props):
-    channel.basic_publish(exchange='logs', routing_key='',body=body, properties = pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
+    channel.basic_publish(exchange='logs', routing_key='', body=body, properties = pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
 
+def sendtopassive(body, props):
+    channel2.basic_publish(exchange='', routing_key='hello', body=body,properties=pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
 
-def main():
+def messages_from_clients():
     channel.queue_declare(queue='hello',durable=True,arguments=client_params)
 
     def callback(ch, method, properties, body):
@@ -35,8 +42,16 @@ def main():
 
 if __name__ == '__main__':
     try:
+       receive_from_clients = threading.Thread(target=messages_from_clients)
+       receive_from_clients.daemon = True
+       receive_from_clients.start()
+       send_to_passive = threading.Thread(target=sendtopassive)
+       send_to_passive.daemon = True
+       send_to_passive.start()
+       receive_from_clients.join()
+       send_to_passive.join()
+               
 
-        main()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
--- passive.py
+++ passive.py
@@ -1,6 +1,9 @@
 import pika
 import sys
 import os
+import threading
+import time
+import uuid
 
 IP = 'localhost'
 server = ''
@@ -10,31 +13,51 @@ cred = pika.PlainCredentials('admin', 'password')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host = "localhost",port=5672,credentials=cred))
 channel = connection.channel()
 
+connection2 = pika.BlockingConnection(pika.ConnectionParameters(host = "active",port=5672,credentials=cred))
+channel2 = connection2.channel()
+
 client_params = {"x-ha-policy": "all"}
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-
+    
 def broadcast(body, props):
-    channel.basic_publish(exchange='logs', routing_key='',body=body, properties= pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
+        channel.basic_publish(exchange='logs', routing_key='',body=body, properties= pika.BasicProperties(correlation_id=props.correlation_id,delivery_mode = 2))
 
+def messages_from_clients():
+        channel.queue_declare(queue='hello',durable=True,arguments=client_params)
 
-def main():
-    channel.queue_declare(queue='hello',durable=True,arguments=client_params)
+        def callback(ch, method, properties, body):
+             print(" [x] %s" % body.decode())
+             broadcast(body, properties)
 
-    def callback(ch, method, properties, body):
-        print(" [x] %s" % body.decode())
-        broadcast(body, properties)
+        channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+        print(channel.channel_number,' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
+    
+def messages_from_active():
+        channel2.queue_declare(queue='hello',durable=True,arguments=client_params)
 
-    channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
-    print(channel.channel_number,' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+        def callback(ch, method, properties, body):
+            print(" [x] %s" % body.decode())
+             
 
+        channel2.basic_consume(
+              queue='hello', on_message_callback=callback, auto_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel2.start_consuming()
 
 if __name__ == '__main__':
+    
     try:
+        receive_from_clients = threading.Thread(target=messages_from_clients)
+        receive_from_clients.daemon = True
+        receive_from_clients.start()
+        receive_from_active = threading.Thread(target=messages_from_active)
+        receive_from_active.daemon = True
+        receive_from_active.start()
+        receive_from_clients.join()
+        receive_from_active.join()
 
-        main()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
